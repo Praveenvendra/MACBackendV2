217c323a9c850cdf8fd4ac78debea0319c363e57
Abhijeet Tehra <tehra.a@mobiusdtaas.ai>
2025-01-31 23:45:08 +0530

    changed index

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.get('/', (req, res) => {
 app.use("/", router); 
 
 const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => {
-//     console.log(`Server running on port ${PORT}`);
-// });
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
 export default app;
